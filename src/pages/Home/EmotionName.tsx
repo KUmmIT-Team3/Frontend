b745a17cd9772dcacd598fec02740c66258fdc75
qkrxogmla <suk05553@naver.com>
2025-06-29 07:25:00 +0900

    [fix]css수정

--- src/pages/Home/EmotionName.tsx
+++ src/pages/Home/EmotionName.tsx
@@ -1,26 +1,23 @@
 type EmotionNameProps = {
-    emotion: string;
-}
+  emotion: string;
+};
 
 const EmotionName = ({ emotion }: EmotionNameProps) => {
-    const colorSet = ['#91ADC6',
-        '#97C0C0',
-        '#F3A9B0',
-        '#FBBD4C',
-        '#ED6956'
-    ]
+  const colorSet = ["#91ADC6", "#97C0C0", "#F3A9B0", "#FBBD4C", "#ED6956"];
 
-    const selectRandColor = () => {
-        const randIndex = Math.floor(Math.random() * 5);
-        return colorSet[randIndex];
-    }
+  const selectRandColor = () => {
+    const randIndex = Math.floor(Math.random() * 5);
+    return colorSet[randIndex];
+  };
 
-    return (
-        <div style={{ backgroundColor: selectRandColor() }} className="flex justify-center items-center w-12 h-7 rounded-[20px] text-white text-xs font-normal font-['SF_Pro'] leading-none">
-            {emotion}
-        </div>
-    )
+  return (
+    <div
+      style={{ backgroundColor: selectRandColor() }}
+      className="flex justify-center items-center w-12 h-7 rounded-[20px] text-white text-xs font-normal  leading-none"
+    >
+      {emotion}
+    </div>
+  );
+};
 
-}
-
-export default EmotionName
+export default EmotionName;
