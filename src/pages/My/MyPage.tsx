4d3a2ac4853f3473cae56fa0e7a26e5dc5e73c21
qkrxogmla <suk05553@naver.com>
2025-06-29 08:04:16 +0900

    패딩추가

--- src/pages/My/MyPage.tsx
+++ src/pages/My/MyPage.tsx
@@ -8,9 +8,9 @@ import UpperNavBar from "../../components/UpperNavBar";
 const MyPage = () => {
   return (
     <div className="flex justify-center">
-      <div className="w-[412px] h-[917px] bg-[#F8F3F4]">
+      <div className="w-[412px] h-[917px] bg-[#F8F3F4] ">
         <UpperNavBar isCanBack={false} text="마이페이지" isLogo={false} />
-        <div className=" bg-[#F8F3F4] mx-auto overflow-y-scroll">
+        <div className=" bg-[#F8F3F4] mx-auto pb-[50px] overflow-y-scroll">
           <MyCard />
           <MyCreatedBands />
           <MyLikedBands />
